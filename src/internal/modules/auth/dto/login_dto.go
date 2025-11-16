531e995dfb69a3834e44ed0ecb1f0bb49f816b6b
Lautaro <lautaro.kaza@gmail.com>
2025-11-15 22:03:19 -0300

    Fix wrong types

--- src/internal/modules/auth/dto/login_dto.go
+++ src/internal/modules/auth/dto/login_dto.go
@@ -4,10 +4,8 @@ import "github.com/lautarok/manosegura/src/infra/validation"
 
 // @name LoginDto
 type LoginDto struct {
-	Username string              `validate:"required,min=6,max=26" example:"usuario.prueba" json:"username"`
-	Password string              `validate:"required,min=3,max=40" json:"password" example:"#Manosegura.1"`
-	Undto    AuthTokenPayloadDto ``
-	Prueba   string              `json:"xd"`
+	Username string `validate:"required,min=6,max=26" example:"usuario.prueba" json:"username"`
+	Password string `validate:"required,min=3,max=40" json:"password" example:"#Manosegura.1"`
 }
 
 func (dto *LoginDto) Validate() error {
